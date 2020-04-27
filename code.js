8b6d068f4069b9bce88fe973123ddb115ca4d015
ADAM ANDREASON <adamandreason@gmail.com>
2020-04-27 00:32:07 -0600

    Site basics completed, more content next

--- code.js
+++ code.js
@@ -1,8 +1,23 @@
-import Plyr from 'plyr';
+let bless4Vid = document.querySelector("#player");
 
-const player = new Plyr('#player', {
-    title: 'Amagi Brilliant Park, featuring EXTRA MAGIC HOUR',
-    controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen'],
-    settings: ['captions', 'quality', 'speed', 'loop'],
-    autoplay: true,
-});
+window.onresize = function (){
+if (window.innerWidth <  562) {
+    console.log(`Sizer running. Width: ${window.innerWidth}`)
+    bless4Vid.setAttribute("poster", "/image_assets/bless4CoverMobile.jpg")
+} else{
+    bless4Vid.setAttribute("poster", "/image_assets/bless4CoverDesktop.jpg")
+}
+}
+
+
+window.onload = function () {
+    console.log("Loaded: check")
+}
+
+// const player = new Plyr('#player', {
+//     title: 'Amagi Brilliant Park, featuring EXTRA MAGIC HOUR',
+//     controls: ['play-large', 'play', 'progress', 'current-time', 'mute', 'volume', 'captions', 'settings', 'pip', 'airplay', 'fullscreen'],
+//     settings: ['captions', 'quality', 'speed', 'loop'],
+//     autoplay: true,
+// });
+
